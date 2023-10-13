90b84ed973ca94e199f6ca68b178b1f3ac6f803e
Shantanu Badgujar <shantanubadgujar2362002@gmail.com>
2023-10-13 17:46:17 +0530

    Created the Layouts and Navigation

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,37 @@
+<section class="bg-white dark:bg-gray-900">
+	<div class="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
+		<div class="font-light text-gray-500 sm:text-lg dark:text-gray-400">
+			<h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
+				Maximize efficiency and productivity
+			</h2>
+			<p class="mb-4">
+				As a Big Data Analyst ourselves, we understand the struggles of searching for the right tools to
+				Generate Data on a daily basis. That's why we've gathered a top-notch selection
+				of high-quality Data Generator in one user-friendly platform, available to developers all
+				around the world for free, forever.
+			</p>
+			<p class="mb-4">Our mission is to be the leading provider of developer tools that empower software developers to overcome obstacles and work more efficiently, streamlining their workflows and increasing productivity. Through our efforts, we envision a future where software development is faster, more accessible, and more innovative than ever before.</p>
+		</div>
+	</div>
+
+	<div class="gap-8 lg:grid lg:grid-cols-12 py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+        <div class="col-span-4 font-light text-gray-500 sm:text-lg dark:text-gray-400">
+		    <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Key features of the platform</h2>
+            <p>The platform offers a curated collection of high-quality developer tools, with a user-friendly interface, free access, and streamlined workflows to enhance developers' efficiency.</p>
+        </div>
+        <div class="col-span-8 gap-8 items-center mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 font-light text-dark sm:text-lg dark:text-white">
+			<div>
+                <h3 class="mb-2 text-xl font-bold dark:text-white">User-Friendly Interface</h3>
+                <p class="text-gray-500 dark:text-gray-400">The platform offers an intuitive, user-friendly interface for seamless tool navigation and enhanced developer productivity.</p>
+            </div>
+			<div>
+                <h3 class="mb-2 text-xl font-bold dark:text-white">Free Access</h3>
+                <p class="text-gray-500 dark:text-gray-400">The developer toolkit platform is free worldwide, ensuring access for all developers, promoting inclusivity and equal opportunities.</p>
+            </div>
+            <div>
+                <h3 class="mb-2 text-xl font-bold dark:text-white">Increased Efficiency</h3>
+                <p class="text-gray-500 dark:text-gray-400">The platform streamlines workflows, saving time and enabling developers to focus on coding and problem-solving.</p>
+            </div>
+        </div>
+	</div>
+</section>

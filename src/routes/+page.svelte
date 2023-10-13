1daa93b9973a6aad27c05b98d99c744afa583459
Shantanu Badgujar <shantanubadgujar2362002@gmail.com>
2023-10-13 21:11:05 +0530

    created home page functionalities

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,14 @@
-  <script>
-   
-  </script>
-  
+<script>
+ import { GradientButton } from 'flowbite-svelte';
+ import { ArrowRightOutline, ShoppingCartSolid } from 'flowbite-svelte-icons';
+</script>
+<div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:px-12">
+  <p class="mb-4 text-4xl  font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Welcome to the ultimate <br />Fake Data Generator!</p>
+    <p class="mb-8 text-lg  font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
+      If you&#39;re in need of realistic but entirely fictional data for various purposes, you&#39;ve come to the right place.</p>
+      <GradientButton color="cyanToBlue" href="/generate">
+        Generate Data<ArrowRightOutline class="w-3.5 h-3.5 ml-2" /> </GradientButton>
+    </div>
+
+
+
